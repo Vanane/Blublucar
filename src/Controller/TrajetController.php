b90d3ffdd9dc396177074e83df3b153dbd220795
Vanane <blackfox@gmail.com>
2020-03-17 19:45:07 +0100

    Ajout login et début de design

--- src/Controller/TrajetController.php
+++ src/Controller/TrajetController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Trajet;
+use App\Entity\Reservation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +17,34 @@ class TrajetController extends AbstractController
         $trajets = $this->getDoctrine()->getRepository(Trajet::class)->getDerniersTrajets();
         return $this->render('trajet/index.html.twig', [
             'trajets' => $trajets
+        ]);
+    }
+    /**
+     * @Route("/trajet/liste", name="trajet.liste")
+     */
+    public function liste()
+    {
+        $trajets = $this->getDoctrine()->getRepository(Trajet::class)->list();
+        
+
+        return $this->render('trajet/liste.html.twig', [
+            'trajets' => $trajets
+        ]);
+    }
+
+    /** 
+     * @Route("/trajet/detail/{id}", name="trajet.detail")
+     * 
+    */
+    public function detail($id)
+    {
+        $trajet = $this->getDoctrine()->getRepository(Trajet::class)->getTrajetParSonId($id);
+        
+        $resas = $this->getDoctrine()->getRepository(Reservation::class)->getPlacesTrajet($id);
+
+        return $this->render('trajet/detail.html.twig', [
+            'trajet' => $trajet,
+            'resastrajet' => $resas
         ]);
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,9 +5,19 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        
+    }
+
+
     /**
      * @param ObjectManager $manager
      *
@@ -17,29 +27,29 @@ class UserFixtures extends Fixture
     {
         $user1 = new User();
        
-        $role = ['Conducteur','Passager'];
+        $role = ['ROLE_USER'];
 
         $user1->setEmail('fred@TF1.fr')
         ->setRoles($role)
-        ->setPassword('123456');
+        ->setPassword($this->passwordEncoder->encodePassword($user1, 'azerty'));
         $manager->persist($user1);
 
         $user2 = new User();
        
-        $role = ['Conducteur'];
+        $role = ['ROLE_USER'];
 
         $user2->setEmail('karine@lebert.com')
         ->setRoles($role)
-        ->setPassword('azerty');
+        ->setPassword($this->passwordEncoder->encodePassword($user2, 'azerty'));
         $manager->persist($user2);
 
         $user3 = new User();
        
-        $role = ['Passager'];
+        $role = ['ROLE_USER'];
 
         $user3->setEmail('Gervaise@Lamartine.com')
         ->setRoles($role)
-        ->setPassword('gervaise');
+        ->setPassword($this->passwordEncoder->encodePassword($user3, 'gervaise'));
         $manager->persist($user3);
 
         $user4 = new User();
@@ -48,19 +58,27 @@ class UserFixtures extends Fixture
 
         $user4->setEmail('Avignon@Dupont.com')
         ->setRoles($role)
-        ->setPassword('blbl');
+        ->setPassword($this->passwordEncoder->encodePassword($user4, 'blbl'));
         $manager->persist($user4);
 
         $user5 = new User();
        
-        $role = ['Conducteur','Passager'];
+        $role = ['ROLE_USER'];
 
         $user5->setEmail('Jeanne@Eleou.fr')
         ->setRoles($role)
-        ->setPassword('456789');
+        ->setPassword($this->passwordEncoder->encodePassword($user5, '456789'));
         $manager->persist($user5);
 
+        $user6 = new User();
+       
+        $role = ['ROLE_ADMIN', 'ROLE_USER'];
 
+        $user6->setEmail('admin@admin.admin')
+        ->setRoles($role)
+        ->setPassword($this->passwordEncoder->encodePassword($user6, 'admin'));
+        $manager->persist($user6);
+        
         $manager->flush();
 
         $this->addReference('user-fred', $user1);
@@ -68,6 +86,7 @@ class UserFixtures extends Fixture
         $this->addReference('user-gervaise', $user3);
         $this->addReference('user-avignon', $user4);
         $this->addReference('user-jeanne', $user5);
+        $this->addReference('user-admin', $user6);
     }
     
 
