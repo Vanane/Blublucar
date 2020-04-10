f6f855e4ae7a8eb18fde8499dc3c9416648f8155
Vanane <blackfox@gmail.com>
2020-04-10 10:54:50 +0200

    Suppression du bundle datatables

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Omines\DataTablesBundle\DataTablesBundle::class => ['all' => true],
 ];
